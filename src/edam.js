865b2cd9705e4032512279f56282872a12b68c6d
Ivan Kuzmin <inkuzm@gmail.com>
2018-06-22 19:04:01 +0300

    and now something completely different

--- src/edam.js
+++ src/edam.js
@@ -1,25 +1,15 @@
-import edamData from './edam/edam_data';
-import edamFormat from './edam/edam_format';
-import edamOperation from './edam/edam_operation';
-import edamTopic from './edam/edam_topic';
+import edam from './edam.json'
 
 export default {
-  clear(type) {
-    let i, l = this.searchData[type].length;
-    for (i = 0; i < l; i += 1) {
-      this.searchData[type][i].show = undefined;
-      this.searchData[type][i].disclosed = undefined;
-    }
-  },
   getNode(type, nodeId) {
     if (nodeId === 'root') {
-      for (var i = 0; i < this.searchData[type][0].children.length; i++) {
-        if (this.searchData[type][0].children[i].indexOf(type) !== -1) {
-          return this.searchData[type][this.index(type)[ this.searchData[type][0].children[i] ]];
+      for (var i = 0; i < this.model[type][0].children.length; i++) {
+        if (this.model[type][0].children[i].indexOf(type) !== -1) {
+          return this.model[type][this.index(type)[ this.model[type][0].children[i] ]];
         }
       }
     } else {
-      return this.searchData[type][this.index(type)[ nodeId ]];
+      return this.model[type][this.index(type)[ nodeId ]];
     }
   },
   index(type) {
@@ -27,7 +17,7 @@ export default {
       return this.indexes[type];
     } else { // else create indexes
       let map = {};
-      let a = this.searchData[type];
+      let a = this.model[type];
       a.map(function (o, i) {
         map[o.id] = i;
       });
@@ -35,11 +25,13 @@ export default {
       return map;
     }
   },
-  searchData: {
-    data: edamData,
-    format: edamFormat,
-    operation: edamOperation,
-    topic: edamTopic
+  model: {
+    data: edam['data'],
+    format: edam['format'],
+    operation: edam['operation'],
+    topic: edam['topic'],
+    schema: edam['schema'],
+    roots: edam['roots']
   },
   indexes: {
     data: null,
--- src/main.js
+++ src/main.js
@@ -1,11 +1,128 @@
-import Vue from 'vue'
-import EdamSelect from './EdamSelect.vue'
+import style from './styles.scss';
 
-new Vue({
-  el: '#app',
-  render: h => h(EdamSelect, {
-    props: {
-      'type': 'operation'
-    }
-  })
-});
+import EDAM from './edam.js';
+
+// it's like compiled template, but manually crafted
+// everything here is manually crafted
+// because performance is crucial
+// when we need to render >1000-nodes trees
+let generateEdamSelectView = function () {
+  let edamSelectWrap = document.createElement('div');
+  edamSelectWrap.className = style['edam-select-wrap'];
+
+  let edamSelectContainer = document.createElement('div');
+  edamSelectContainer.className = style['edam-select-container'];
+
+  let edamSelectInputWrap = document.createElement('div');
+  edamSelectInputWrap.className = style['edam-select-input-wrap'];
+
+  let input = document.createElement('input');
+  input.type = 'text';
+
+  let edamSelectMenu = document.createElement('div');
+  edamSelectMenu.className = style['edam-select-menu-wrap'];
+
+  edamSelectWrap.appendChild(edamSelectContainer);
+  edamSelectWrap.appendChild(edamSelectMenu);
+
+  edamSelectContainer.appendChild(edamSelectInputWrap);
+  edamSelectInputWrap.appendChild(input);
+
+  return edamSelectWrap;
+};
+
+let generateEdamSelectMenuView = function () {
+  let edamSelectMenu = document.createElement('ul');
+  edamSelectMenu.className = style['edam-select-menu'];
+  return edamSelectMenu;
+};
+
+let generateEdamSelectMenuItemView = function (termText) {
+  let treeMenu = document.createElement('li');
+  treeMenu.className = style['tree-menu'];
+
+  let labelWrapper = document.createElement('div');
+  labelWrapper.className = style['label-wrapper'];
+
+  let labelIndent = document.createElement('div');
+  labelIndent.className = style['label-indent'];
+
+  let triangleWrap = document.createElement('span');
+  triangleWrap.className = style['triangle-wrap'];
+
+  let triangle = document.createElement('i');
+  triangle.className = style['triangle'];
+
+  let term = document.createElement('span');
+  term.className = style['term'];
+
+  // let ul = document.createElement('ul');
+
+  treeMenu.appendChild(labelWrapper);
+  labelWrapper.appendChild(labelIndent);
+
+  labelIndent.appendChild(triangleWrap);
+  labelIndent.appendChild(term);
+  triangleWrap.appendChild(triangle);
+
+  let text = document.createTextNode(termText);
+  term.appendChild(text);
+
+  // treeMenu.appendChild(ul);
+
+  return treeMenu;
+};
+
+// create root element and attach to "#app"
+// later there will be an initializer
+let edamSelectView = generateEdamSelectView();
+document.getElementById('app').appendChild(edamSelectView);
+
+let edamSelectMenuView = generateEdamSelectMenuView();
+// yes, optimisation is ugly
+// but fassst
+edamSelectView.getElementsByClassName(style['edam-select-menu-wrap'])[0]
+  .appendChild(edamSelectMenuView);
+
+
+let i;
+let type = 'data';
+for (i = 0; i < EDAM.model[type].length; i += 1) {
+  let term = EDAM.model[type][i];
+
+  let v = generateEdamSelectMenuItemView(term[1]);
+
+  edamSelectView.getElementsByClassName(style['edam-select-menu'])[0]
+    .appendChild(v);
+}
+console.log(i)
+/*
+<div class="edam-select-wrap" :class="classObject">
+  <div class="edam-select-container">
+    <div class="edam-select-input-wrap">
+      <input type="text" v-bind:placeholder="placeholder" @input="search">
+    </div>
+  </div>
+  <ul class="edam-select-menu" v-if="status !== 'filteredButNothingWasFound' && status !== 'closed'">
+
+  <li
+  is="tree-menu"
+class="edam-select-menu-item"
+
+:nodes="tree.children"
+:depth="0"
+:label="tree.label"
+:id="tree.id"
+
+:disclosureResults="disclosureResults"
+:searchResults="searchResults"
+  ></li>
+  </ul>
+
+  <div v-if="status === 'filteredButNothingWasFound'" class="nothing-found">
+  Nothing was found!
+</div>
+
+</div>
+
+ */

757e8008067becac21605ec76777ceac03daad81
Ivan Kuzmin <inkuzm@gmail.com>
2018-06-29 12:57:43 +0300

    add info

--- src/main.js
+++ src/main.js
@@ -10,15 +10,19 @@ class EdamSelect {
   constructor(sel, params) {
     this.id = ++EdamSelect.id;
 
-    this.el = this.generateEdamSelectView();
-    document.querySelector(sel).appendChild(this.el);
-
     if (['format', 'data', 'topic', 'operation'].indexOf(params.type) !== -1) {
       this.type = params.type;
     } else {
       throw new Error("Unknown type. Should be one of [ format | data | topic | operation ].");
     }
-    this.initDepth = params.initDepth || 1;
+
+    if (params.initDepth === undefined) {
+      this.initDepth = 1;
+    } else if (params.initDepth === 0) {
+      this.initDepth = +Infinity;
+    } else {
+      this.initDepth = params.initDepth;
+    }
 
     this.edam = new EDAM(this.type);
 
@@ -28,14 +32,20 @@ class EdamSelect {
     this.index = this.edam.index(); // singleton for returning indexes
 
     // underscore for such accessors (see below)
-    this._status = 'opened';
+    this.inline = params.inline;
+    if (this.inline) {
+      this._status = 'opened';
+    } else {
+      this._status = 'closed';
+    }
+    this.oldStatus = undefined;
 
     console.time('Fuse init');
     this.fuse = new Fuse(this.model, {
-      keys: ['1'],
+      keys: ['1', '2', '3'],
       shouldSort: true,
       includeMatches: true,
-      threshold: 0.3,
+      threshold: 0.2,
       location: 0,
       distance: 100,
       maxPatternLength: 32,
@@ -54,6 +64,9 @@ class EdamSelect {
     // it could be refactored to Change object
     this.changed = [];
 
+    this.el = this.generateEdamSelectView();
+    document.querySelector(sel).appendChild(this.el);
+
     this.init();
   }
 
@@ -61,48 +74,86 @@ class EdamSelect {
     return this._status;
   }
 
-  set status(value){ // wow so valid
+  set status(newStatus){ // wow so valid
     if (['opened', 'filtered', 'filteredButNothingWasFound',
-        'unfiltered', 'closed', 'focused', 'selected'].indexOf(value) === -1) {
+        'unfiltered', 'closed', 'focused', 'selected'].indexOf(newStatus) === -1) {
       // custom error messages are recognisable by the absence of explanation marks
       throw new Error("Unknown status"); // dot is optional tho
     } else {
-
-      if (this._status !== value) {
-        let i, l = this.changed.length;
-        for (i = 0; i < l; i += 1) {
-          let nodeId = this.changed.pop();
-          let term = this.model[this.index[nodeId]];
-          term['disclosed'] = undefined;
-          term['shown'] = undefined;
-        }
+      this.oldStatus = this._status;
+      switch (this.oldStatus) {
+        case 'opened': // current
+          switch (newStatus) {
+            case 'filtered': // new
+              this.resetChanges();
+              break;
+          }
+          break;
+        case 'filtered': // current
+          switch (newStatus) {
+            case 'unfiltered':
+              break;
+          }
+          break;
+        case 'filteredButNothingFound': // current
+          break;
+        case 'unfiltered': // current
+          switch (newStatus) {
+            case "filtered":
+              this.resetChanges();
+              break;
+          }
+        case 'closed': // current
+          break;
+        case 'focused': // current
+          break;
+        case 'selected': // current
+          break;
       }
 
-      this._status = value;
+      this._status = newStatus;
 
       this.init();
+    }
+  }
+
+  resetChanges() {
+    let i, l = this.changed.length;
+    for (i = 0; i < l; i += 1) {
+      let nodeId = this.changed.pop();
+      let term = this.model[this.index[nodeId]];
+      term['disclosed'] = undefined;
+      term['shown'] = undefined;
     }
   }
 
   init() {
     console.time('Tree rendering');
 
-    let idx = this.index[this.rootId];
-    let term = this.model[idx];
+    if (this.status !== 'closed') {
+      let idx = this.index[this.rootId];
+      let term = this.model[idx];
 
-    let treeMenu = new TreeMenu(term, {
-      initDepth: this.initDepth,
-      depth: 0,
-      type: this.type,
-      status: this.status,
-      edam: this.edam,
-      id: this.id,
-      searchResults: this.searchResults,
-      disclosureResults: this.disclosureResults,
-      // changed: this.changed -- no, this is child - parent channel, it should be impl. with events...
-    });
+      let treeMenu = new TreeMenu(term, {
+        initDepth: this.initDepth,
+        depth: 0,
+        type: this.type,
+        status: this.status,
+        edam: this.edam,
+        id: this.id,
+        searchResults: this.searchResults,
+        disclosureResults: this.disclosureResults,
+        fuseResults: this.fuseResults,
+        // changed: this.changed -- no, this is child - parent channel, it should be impl. with events...
+      });
 
-    this.append(treeMenu);
+      this.append(treeMenu);
+    } else {
+      let treeMenu = this.el.getElementsByClassName(style['tree-menu'])[0];
+      if (treeMenu) {
+        treeMenu.remove();
+      }
+    }
 
     console.timeEnd('Tree rendering');
   }
@@ -126,21 +177,51 @@ class EdamSelect {
     let edamSelectMenu = document.createElement('ul');
     edamSelectMenu.className = style['edam-select-menu'];
 
+    let edamSelectArrowWrap = document.createElement('div');
+    edamSelectArrowWrap.className = style['edam-select-arrow-wrap'];
+
+    let edamSelectArrow = document.createElement('div');
+    edamSelectArrow.className = style['edam-select-arrow'];
+
+    edamSelectArrowWrap.appendChild(edamSelectArrow);
+
     edamSelectWrap.appendChild(edamSelectContainer);
     edamSelectWrap.appendChild(edamSelectMenuWrap);
 
     edamSelectContainer.appendChild(edamSelectInputWrap);
     edamSelectInputWrap.appendChild(input);
 
+    edamSelectContainer.appendChild(edamSelectArrowWrap);
+
     edamSelectMenuWrap.appendChild(edamSelectMenu);
 
+    if (this.status === 'opened' || this.status === 'filtered' || this.status === 'unfiltered') {
+      edamSelectWrap.classList.add(style['is-open']);
+    }
+
     // event handlers will be here
+    edamSelectArrowWrap.addEventListener('click', (e) => {
+      if (this.status === 'opened' || this.status === 'filtered' || this.status === 'unfiltered') {
+        edamSelectWrap.classList.remove(style['is-open']);
+        this.status = 'closed';
+      } else {
+        edamSelectWrap.classList.add(style['is-open']);
+        this.status = this.oldStatus;
+      }
+      input.focus();
+      this.init();
+    });
+
     input.addEventListener('input', (e) => {
       this.search(e);
     });
 
     document.addEventListener(`edam:${this.id}:change`, (e) => {
       this.changed.push(e.detail);
+    });
+
+    document.addEventListener(`edam:${this.id}:status`, (e) => {
+      this.updateStatus();
     });
 
     return edamSelectWrap;
@@ -151,6 +232,7 @@ class EdamSelect {
       this.searchForReal(e);
     } else {
       clearTimeout(this.timeout);
+      this.status = "unfiltered";
     }
   }
   searchForReal(e) {
@@ -160,25 +242,28 @@ class EdamSelect {
       this.disclosureResults = [];
       this.searchResults = [];
 
-      let results = this.fuse.search(e.target.value);
+      this.fuseResults = this.fuse.search(e.target.value);
 
-      results.forEach((result) => {
+      this.fuseResults.forEach((result) => {
         this.searchResults.push(result.item[0]);
         this.disclosureResults.push(...this.addAncestors(result.item[0]));
       });
 
       this.disclosureResults = [ ...new Set(this.disclosureResults) ];
 
-      if (this.searchResults.length > 0) {
-        this.status = "filtered";
-      } else {
-        this.status = "filteredButNothingWasFound";
-      }
+      let event = new Event('edam:' + this.id + ':status');
+      document.dispatchEvent(event);
 
-      console.log("disclosure:", this.disclosureResults);
-      console.log("search:", this.searchResults);
       console.timeEnd('Search');
     }, 500);
+  }
+
+  updateStatus() {
+    if (this.searchResults.length > 0) {
+      this.status = "filtered";
+    } else {
+      this.status = "filteredButNothingWasFound";
+    }
   }
 
   addAncestors(nodeId) {
@@ -234,20 +319,12 @@ class TreeMenu {
     this.id = params.id;
     this.disclosureResults = params.disclosureResults;
     this.searchResults = params.searchResults;
-
-    // this.attachAccessors(this.term);
-    // now can do this:
-    // this.term.set('label', 'test');
-    // this.term.get('label');
-    // by the way, it's good time to fulfill the model with custom fields
+    this.fuseResults = params.fuseResults;
 
     this.el = this.generateEdamSelectMenuItemView();
     return this.el;
   }
 
-  // never look inside this function
-  // now it's fine
-  // it was nice idea, but... whatever
   attachAccessors(term) {
     term.get = function(what) { // this is more elegant, but could be slower
       let idx = this.edam.getSchema().indexOf(what);
@@ -312,6 +389,13 @@ class TreeMenu {
     */
   }
 
+  triggerChangeEvent() {
+    let event = new CustomEvent('edam:' + this.id + ':change', {
+      detail: this.term[0]
+    });
+    document.dispatchEvent(event);
+  }
+
   generateEdamSelectMenuItemView() {
     let treeMenu = document.createElement('li');
     treeMenu.className = style['tree-menu'];
@@ -334,11 +418,15 @@ class TreeMenu {
       let term = document.createElement('span');
       term.className = style['term'];
 
+      let info = document.createElement('div');
+      info.className = style['info'];
+
       treeMenu.appendChild(labelWrapper);
       labelWrapper.appendChild(labelIndent);
 
       labelIndent.appendChild(triangleWrap);
       labelIndent.appendChild(term);
+      labelIndent.appendChild(info);
 
       triangleWrap.appendChild(triangle);
 
@@ -346,14 +434,78 @@ class TreeMenu {
       term.appendChild(text);
 
       // to save operations on access to elements we can add handlers right here?
-      triangleWrap.addEventListener('click', () => {
-        let event = new CustomEvent('edam:' + this.id + 'change', {
-          detail: this.term[0]
-        });
-        document.dispatchEvent(event);
+      info.addEventListener('click', () => {
+        let details = labelWrapper.getElementsByClassName(style['details'])[0];
+        if (details) {
+          details.remove();
+        } else {
+          details = document.createElement('div');
+          details.className = style['details'];
 
+          let i, l = this.term[3].length;
+          if (l > 0) {
+            let definitionsTitle = document.createElement('div');
+            definitionsTitle.className = style['title'];
+            let definitionsTitleText = document.createTextNode("Definitions:");
+            definitionsTitle.appendChild(definitionsTitleText);
+            let definitionsWrap = document.createElement('div');
+            definitionsWrap.className = style['definitions'];
+            for (i = 0; i < l; i += 1) {
+              let definitionmItem =  document.createElement('div');
+              let definitionText = document.createTextNode(this.term[3][i]);
+              definitionmItem.appendChild(definitionText);
+              definitionsWrap.appendChild(definitionmItem);
+            }
+            details.appendChild(definitionsTitle);
+            details.appendChild(definitionsWrap);
+          }
+
+
+          l = this.term[2].length;
+          if (l > 0) {
+            let synonymsWrap = document.createElement('ul');
+            let synonymsTitle = document.createElement('div');
+            synonymsTitle.className = style['title'];
+            let synonymsTitleText = document.createTextNode("Synonyms:");
+            synonymsTitle.appendChild(synonymsTitleText);
+            for (i = 0; i < l; i += 1) {
+              let synonymItem =  document.createElement('li');
+              let synonymText = document.createTextNode(this.term[2][i]);
+              synonymItem.appendChild(synonymText);
+              synonymsWrap.appendChild(synonymItem);
+            }
+            details.appendChild(synonymsTitle);
+            details.appendChild(synonymsWrap);
+          }
+
+          let linkTitle = document.createElement('p');
+          linkTitle.className = style['title'];
+          let linkTitleText = document.createTextNode("URL:");
+          linkTitle.appendChild(linkTitleText);
+          let link = document.createElement('a');
+          let url = `http://edamontology.org/${this.type}_${this.term[0]}`;
+          let linkText = document.createTextNode(url);
+          link.appendChild(linkText);
+          link.href = url;
+          details.appendChild(linkTitle);
+          details.appendChild(link);
+
+          labelIndent.appendChild(details);
+
+          setTimeout(() => {details.classList.add(style['show']);}, 10);
+        }
+      });
+
+      triangleWrap.addEventListener('click', () => {
+        this.triggerChangeEvent();
         this.disclose();
       });
+
+      labelWrapper.addEventListener('mouseover', () => {
+        labelWrapper.classList.add(style['selection']);
+      });
+
+
 
       // styles go here for the same reason
       labelIndent.style.marginLeft = (this.depth - 0.5) + 'em';
@@ -366,8 +518,53 @@ class TreeMenu {
         triangle.classList.add(style['opened']);
       }
 
-      if (this.isInSearchResults()) {
+      let whereInSearchResults = this.whereInSearchResults();
+      if (whereInSearchResults > -1) {
+
         labelIndent.classList.add(style['highlighted']);
+
+        let i, l = this.fuseResults[whereInSearchResults].matches.length;
+        let j, ll, start, end, innerHTML;
+        let found = false;
+        for (i = 0; i < l; i += 1) {
+          if (this.fuseResults[whereInSearchResults].matches[i].key === "1") {
+            ll = this.fuseResults[whereInSearchResults].matches[i].indices.length;
+            for (j = 0; j < ll; j += 1) {
+              start = this.fuseResults[whereInSearchResults].matches[i].indices[j][0];
+              end = this.fuseResults[whereInSearchResults].matches[i].indices[j][1];
+
+              if (end - start > 0) {
+                innerHTML = this.term[1].substring(0, start) +
+                  "<strong>" + this.term[1].substring(start, end + 1) + "</strong>" +
+                  this.term[1].substring(end + 1, this.term[1].length);
+
+                term.innerHTML = innerHTML;
+                found = true;
+              }
+            }
+          } else if (this.fuseResults[whereInSearchResults].matches[i].key === "2" && !found) {
+            let span = document.createElement("span");
+            span.className = style["by"];
+            let spanText = document.createTextNode("(by synonyms)");
+            span.appendChild(spanText);
+            term.appendChild(span);
+          } else if (this.fuseResults[whereInSearchResults].matches[i].key === "3" && !found) {
+            let bySynonyms = term.getElementsByClassName(style["by"]);
+            if (bySynonyms.length > 0) {
+              let span = document.createElement("span");
+              span.className = style["by"];
+              let spanText = document.createTextNode("(by synonyms and definition)");
+              span.appendChild(spanText);
+              term.replaceChild(span, bySynonyms[0]);
+            } else {
+              let span = document.createElement("span");
+              span.className = style["by"];
+              let spanText = document.createTextNode("(by definition)");
+              span.appendChild(spanText);
+              term.appendChild(span);
+            }
+          }
+        }
       } else {
         labelIndent.classList.remove(style['highlighted']);
       }
@@ -388,6 +585,7 @@ class TreeMenu {
           id: this.id,
           searchResults: this.searchResults,
           disclosureResults: this.disclosureResults,
+          fuseResults: this.fuseResults,
         }));
       });
     }
@@ -402,7 +600,16 @@ class TreeMenu {
   }
 
   getChildren() {
-    return this.term[4].map(id => this.edam.getById(id)); // term[4] is children field
+      return this.term[4].map(id => this.edam.getById(id)).sort((a, b) => {
+        if (a[1] > b[1]) { // ah yes, need to sort it...
+          return 1;
+        } else if (a[1] < b[1]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    // term[4] is children field
   }
 
   isShown() {
@@ -411,10 +618,12 @@ class TreeMenu {
         return true;
       case "filtered":
         if (this.term['shown'] === undefined) {
-          return this.isInDisclosureResults() || this.isInSearchResults();
+          return this.isInDisclosureResults() || (this.whereInSearchResults() > -1);
         } else {
           return this.term['shown'];
         }
+      case "unfiltered":
+        return true;
       default:
         throw new Error("Huh, unknown status, how could this happen?");
     }
@@ -429,6 +638,12 @@ class TreeMenu {
           return this.term['disclosed'];
         }
       case "filtered":
+        if (this.term['disclosed'] === undefined) {
+          return this.isInDisclosureResults();
+        } else {
+          return this.term['disclosed'];
+        }
+      case "unfiltered":
         if (this.term['disclosed'] === undefined) {
           return this.isInDisclosureResults();
         } else {
@@ -462,6 +677,7 @@ class TreeMenu {
       id: this.id,
       searchResults: this.searchResults,
       disclosureResults: this.disclosureResults,
+      fuseResults: this.fuseResults,
     }), this.el);
 
     console.timeEnd('Disclose');
@@ -471,8 +687,8 @@ class TreeMenu {
     return this.disclosureResults.indexOf(this.term[0]) !== -1;
   }
 
-  isInSearchResults() {
-    return this.searchResults.indexOf(this.term[0]) !== -1;
+  whereInSearchResults() {
+     return this.searchResults.indexOf(this.term[0]);
   }
 
   remove() {
@@ -484,58 +700,8 @@ console.time('Application');
 
 let edamSelect = new EdamSelect('#app', {
   initDepth: 1,
-  type: 'data'
+  type: 'data',
+  inline: true,
 });
 
 console.timeEnd('Application');
-/*
-// okay, to generate fully disclosed tree we need
-console.time('someFunction');
-let i, type = 'data', length = EDAM.model[type].length;
-let model = EDAM.model[type];
-
-let rootId = EDAM.model.roots[type];
-let schema = EDAM.model.schema;
-
-edamSelect.append(v);
-
-for (i = 0; i < length; i += 1) {
-  let term = model[i];
-
-  let v = new TreeMenu(term);
-
-  edamSelect.append(v);
-}
-console.timeEnd('someFunction');
-
-console.log(EDAM.model[type]);
-
-<div class="edam-select-wrap" :class="classObject">
-  <div class="edam-select-container">
-    <div class="edam-select-input-wrap">
-      <input type="text" v-bind:placeholder="placeholder" @input="search">
-    </div>
-  </div>
-  <ul class="edam-select-menu" v-if="status !== 'filteredButNothingWasFound' && status !== 'closed'">
-
-  <li
-  is="tree-menu"
-class="edam-select-menu-item"
-
-:nodes="tree.children"
-:depth="0"
-:label="tree.label"
-:id="tree.id"
-
-:disclosureResults="disclosureResults"
-:searchResults="searchResults"
-  ></li>
-  </ul>
-
-  <div v-if="status === 'filteredButNothingWasFound'" class="nothing-found">
-  Nothing was found!
-</div>
-
-</div>
-
- */
